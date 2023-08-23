ef0b50315275b73cd40490bf2922cf9c31ee2833
new-crystal <oj3339@naver.com>
2023-08-23 16:07:27 +0900

    mobile_onsite 수정

--- application/views/success.php
+++ application/views/success.php
@@ -7,45 +7,93 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+KR&display=swap" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
+
+
+    <style>
+    @font-face {
+        font-family: NanumSquare;
+        src: url("../../../assets/font/NanumSquare.otf");
+    }
+
+    body {
+        font-family: NanumSquare;
+    }
+
+    p {
+        line-height: 27px;
+    }
+
+    #success_container {
+        width: 42%;
+        margin: 0 auto;
+        text-align: center;
+        display: flex;
+        flex-direction: column;
+        font-weight: 500;
+    }
+
+    .text_box {
+        font-size: 1.25rem;
+        line-height: 1.75rem;
+        margin-top: 5rem;
+    }
+
+    @media screen and (max-width:500px) {
+        #success_container {
+            width: 100% !important;
+            height: 100% !important;
+        }
+
+        .text_box {
+            margin-top: 10rem;
+            font-size: 2.25rem;
+            line-height: 2.5rem;
+        }
+
+        p {
+            line-height: 80px;
+        }
+
+        .pre_btn {
+            width: 25%;
+            margin-top: 100px;
+            font-size: 2rem;
+        }
+    }
+    </style>
 </head>
 
-<style>
-@font-face {
-    font-family: NanumSquare;
-    src: url("../../../assets/font/NanumSquare.otf");
-}
-
-body {
-    font-family: NanumSquare;
-}
-</style>
-
 <body>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <div class="w-full h-screen flex items-center justify-center">
-        <div class="w-5/12 h-5/6 m-auto border border-blue-800 text-center font-medium flex flex-col justify-between">
-            <div class="w-full h-80">
+    <div id="success_container">
+        <div>
+            <div class="w-full h-80 sm:h-1/3">
                 <img src="../../assets/images/access_header.png" class="h-full w-full" />
             </div>
             <div>
-                <div class="text-zinc-400 text-xl">
+                <!-- <div class="text-zinc-400 text-xl sm:text-4xl">
                     <p>Field registration completed.</p>
                     <p>After paying the registration fee of <?php if (isset($fee)) echo $fee; ?></p>
                     <p>at the registration desk</p>
-                    <p>Please enter after receiving the pass.</p>
-                </div>
-                <div class="text-zinc-400 text-xl mt-10">
+                    <p>Please enter after receiving the nametag.</p>
+                </div> -->
+                <div class="text_box text-zinc-400">
                     <p>현장 등록이 완료되었습니다</p>
-                    <p>등록데스크에서 등록비 <?php if (isset($fee)) echo $fee; ?>원 결제 후</p>
-                    <p>출입증을 수령하여 입장해주세요.</p>
+                    <p>등록데스크에서 등록비 <span class="text-blue-900"><?php if (isset($fee)) echo $fee; ?>원</span> 결제 후</p>
+                    <p>네임택을 수령하여 입장해주세요.</p>
+                </div>
+                <div class="text_box text-blue-900 mt-10">
+                    <p>(대한내분비학회)</p>
+                    <p>국민은행</p>
+                    <p>803-25-0006-851</p>
                 </div>
             </div>
-            <div class="text-red-500 font-semibold mb-5">
-                <p>※ 현장등록 참석자 분들은 수료증이 필요 하신 경우 학회 운영사무국으로 연락 부탁드립니다.</p>
+            <div class="text_box text-red-500 font-semibold my-20">
+                <p>※ 현장등록 참석자 분들은 수료증이 필요 하신 경우 <br>학회 운영사무국으로 연락 부탁드립니다.</p>
                 <p>(현장 등록 참석자 학회 사이트 내에 다운로드 불가)</p>
             </div>
             <div>
-                <button class="py-2 px-3 border mb-3 hover:bg-slate-100" onclick="prev()">이전으로</button>
+                <button class="pre_btn py-4 px-5 border mb-3 hover:bg-slate-100" onclick="prev()">이전으로</button>
             </div>
         </div>
     </div>
---
+++ application/views/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    theme: {
+      extend: {
+        sm: {
+          width: "390px",
+        }
+      }
+    }
+  }
