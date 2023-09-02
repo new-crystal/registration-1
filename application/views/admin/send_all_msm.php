3ed0081b91dec8d093e88a2166d51d168c3acd5d
new-crystal <oj3339@naver.com>
2023-09-02 15:14:14 +0900

    msm 공백 추가

--- application/views/admin/send_all_msm.php
+++ application/views/admin/send_all_msm.php
@@ -45,7 +45,7 @@ if ($err) {
                 'phone' =>  $item['phone'],
                 'callback' => '01090224867',
                 'message' => '
-안녕하세요. ' . $item['nick_name'] . '선생님.'  .
+안녕하세요. ' . $item['nick_name'] . ' 선생님.'  .
                     '
 본 연수강좌에 참석 해주셔서 감사합니다.
             
