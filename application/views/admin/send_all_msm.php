6a20cbbc170e8855f99e75467e123f4b8aa5bd61
new-crystal <oj3339@naver.com>
2024-02-29 17:28:02 +0900

    [240229] edit mail, msm

--- application/views/admin/send_all_msm.php
+++ application/views/admin/send_all_msm.php
@@ -43,25 +43,36 @@ if ($err) {
             CURLOPT_CUSTOMREQUEST => "POST",
             CURLOPT_POSTFIELDS => array(
                 'phone' =>  $item['phone'],
-                'callback' => '01090224867',
+                'callback' => '01065492506',
                 'message' => '
-안녕하세요. ' . $item['nick_name'] . ' 선생님.'  .
-                    '
-본 연수강좌에 등록 해주셔서 감사합니다.
-            
-연수강좌 관련 사전 안내 드립니다.
-- 행사명 : 대한내분비학회 2023년 개원의 연수강좌
-- 일시 : 2023년 9월 3일(일), 09:20~
-- 장소 : SC컨벤션 국제회의실(B1) (서울 강남구 테헤란로7길 22 과학기술회관)
-- 등록데스크 오픈 : 2023년 9월 3일(일), 09:00~
-            
-# 금일 발송되는 문자는 현장에서 참석자들의 빠르고, 원활한 등록을 위한 안내 문자입니다.
-# 등록데스크에 방문하여 위 QR코드를 제시 후 네임택을 수령 부탁드립니다.
-# 위 QR코드는 출석 태그 QR코드가 아니며, 출석 태그는 네임택 수령 후 네임택 QR코드을 사용 부탁 드립니다.
-            
-기타 문의 사항은 02-2285-2579로 연락 주시면 감사하겠습니다.
-            
--운영사무국 드림-
+안녕하십니까,
+제59차 대한비만학회 춘계학술대회 운영사무국 입니다. 
+
+먼저 본 학술대회에 등록해 주셔서 감사 드립니다.
+현장 방문 시, 원활한 등록을 위해 아래와 같이 안내를 드리오니 사전에 확인 부탁드립니다.  
+                            
+[제59차 대한비만학회 춘계학술대회] 
+- 날짜: 2024년 3월 8일(금) ~ 9일(토)
+- 장소: 그랜드 워커힐 서울
+- 오시는 길: https://kosso.org/main/venue.php
+- 등록데스크 위치: 비스타홀(B2) 로비
+- 등록데스크 운영시간: 3월 8일(금), 13:00-18:30 / 3월 9일(토), 06:30-18:00
+                
+[등록 QR 코드]
+*   등록데스크에 방문하여 하기 QR 코드를 제시 후 네임택을 수령 부탁드립니다.
+                
+[기타 안내사항]
+*   본 학술대회에서는 인쇄된 초록집을 제공하지 않습니다. 참석에 필요한 모든 정보는 어플리케이션에서 확인하실 수 있습니다.
+*   어플리케이션 상세 기능: 등록 및 출결, 프로그램 및 실시간 세션 확인, 연사 정보 확인, 초록집 다운로드
+                
+-   애플앱스토어: https://apps.apple.com/kr/app/ksso2024/id6476071605
+-   구글플레이: https://play.google.com/store/apps/details?id=com.intoon.ksso
+-   앱스토어/구글 스토어에서 KSSO2024를 검색하세요!
+                
+감사합니다.
+제59차 대한비만학회 춘계학술대회 운영사무국 드림. 
+
+TEL: 82-2-2285-2582 / E-mail: ksso@into-on.com
                 ', 'refkey' => 'RESTAPITEST1548722798', 'subject' => '대한내분비학회 2023년 개원의 연수강좌 안내', 'image_cnt' =>
                 '1', '
              images0' => new CURLFILE('assets/images/QR/qrcode_' . $item['registration_no'] . '.jpg')
@@ -109,7 +120,7 @@ if ($err) {
         <div class="w-2/4 h-3/4 bg-orange-500 flex flex-col items-center justify-center">
             <h1 class="text-white font-semibold text-3xl">MMS 전송이 실패하였습니다.</h1>
             <p class="text-xl font-semibold mt-5"> <?= $error_msg ?> </p>
-            <a href="/admin/qr_user"><button class="bg-white bg-orange-500 p-3 font-semibold rounded">뒤로
+            <a href="/admin/qr_user"><button class=" bg-orange-500 p-3 font-semibold rounded">뒤로
                     가기</button></a>
         </div>
     <?php endif; ?>
