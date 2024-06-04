02d23ef054e97c703ccf9c3d017f38824cbbdaa5
new-crystal <oj3339@naver.com>
2024-06-04 11:17:22 +0900

    [240604] edit qr_layout_all

--- application/views/admin/qr_layout.php
+++ application/views/admin/qr_layout.php
@@ -4,15 +4,18 @@
 
 <style>
     @page {
-        size: 10cm 24cm;
+        /* size: 10cm 24cm; */
+        size: 794px 960px;
         margin: 0;
     }
 
         
     @media print {
             #printThis {
-            width: 10cm;
-            height: 24cm;
+            /* width: 10cm;
+            height: 24cm; */
+            width: 794px;
+            height: 960px;
             margin: 0;
             padding: 0;
         }
@@ -37,8 +40,10 @@
     }
 
     #printThis {
-        width: 10cm;
-        height: 24cm;
+        /* width: 10cm;
+        height: 24cm; */
+        width: 794px;
+        height: 960px;
         margin: 0;
         padding: 0;
     }
@@ -70,37 +75,37 @@
                 $lang = preg_match("/[\xE0-\xFF][\x80-\xFF][\x80-\xFF]/", $users['nick_name']);
                 $nicknameLength = mb_strlen($users['nick_name'], "UTF-8");
                 $orgLength = mb_strlen($users['org_nametag'], "UTF-8");
-                $reg_num = explode("-", $users['registration_no'])[1];
+                // $reg_num = explode("-", $users['registration_no'])[1];
 
                 echo '<div class="a4_area">';
                 echo '<div class="bg_area">';
                 echo '<div class="txt_con">';
-                echo '<div class="reg_num_1">' . $reg_num . '</div>';
+                // echo '<div class="reg_num_1">' .$users['registration_no'] . '</div>';
                 //성함 조건식 
                 //1. 3글자 //영문 letter_spacing = 0, 한글 = 10
-                if ($nicknameLength <= 3) {
-                    echo '<div class="nick_name" id="nick_name" style="letter-spacing: ' . $letter_spacing . ';margin-left: ' . $letter_spacing . ';">' . $users['nick_name'] . '</div>';
-                } 
-                //2. 4 ~ 6 글자 //영문 letter_spacing = 0, 한글 = 10
-                else if ($nicknameLength > 3 && $nicknameLength <= 6) {
-                    echo '<div class="small_nickname" id="nick_name" style="letter-spacing: ' . $letter_spacing . ';margin-left: ' . $letter_spacing . ';">' . $users['nick_name'] . '</div>';
-                } 
-                //3. 7 ~ 16 글자 //영문 letter_spacing = 0, 한글 = 10
-                else if($nicknameLength > 6 && $nicknameLength <= 16){
-                    echo '<div class="small_small_nickname" id="nick_name" style="letter-spacing: ' . $letter_spacing . ';margin-left: ' . $letter_spacing . ';">' . $users['nick_name'] . '</div>';
-                }
-                //4. 17 글자부터 ~ //영문 letter_spacing = 0, 한글 = 10
-                else if($nicknameLength > 16){
-                    echo '<div class="small_small_small_nickname" id="nick_name" style="letter-spacing: ' . $letter_spacing . ';margin-left: ' . $letter_spacing . ';">' . $users['nick_name'] . '</div>';
-                }
+                echo '<div class = "box_1_area">';
+              
+                echo '<div class="nick_name" id="nick_name" style="letter-spacing: ' . $letter_spacing . ';margin-left: ' . $letter_spacing . ';">' . $users['nick_name'] . '</div>';
+                
                 echo '<div class="org small_org" id="org">' . $users['org_nametag'] . '</div>';
-                echo '<div id="qrcode" class=""><img src="/assets/images/QR/qrcode_' . $users['registration_no'] . '.jpg"></div>';
+                echo '<div id="qrcode" class=""><img src="/assets/images/QR/qrcode_' . $users['registration_no'] . '.jpg"></div></div>';
+
                 echo '<div class="small_small_receipt">';
-                echo '<div class="receipt receipt_price">' . number_format($users['fee']) . '</div>';
+
 
                 //학회팀 요청 영수증 성함 한글일 때 letter_spacing = 5
-                echo '<div class="receipt receipt_name" style="letter-spacing: ' . $letter_spacing_receipt . ';">' . $users['nick_name'] . '</div>';
-                echo '<div class="receipt receipt_num">' . $reg_num . '</div>';
+                echo '<div class = "box_2_area">';
+                echo '<div class="receipt receipt_num">' .$users['registration_no'] . '</div>';
+                echo '<div class="receipt receipt_name">' . $users['nick_name'] . '</div>';
+                echo '<div class="receipt receipt_price">' . $users['fee']. '</div></div>';
+
+
+                echo '<div class ="box_3_area">';
+                echo '<div class="receipt receipt_num">' .$users['registration_no'] . '</div>';
+                echo '<div class="receipt receipt_name">' . $users['nick_name'] . '</div>';
+                echo '<div class="receipt ln">' . $users['licence_number']. '</div>';
+                echo '<div class="receipt sn">' . $users['specialty_number']. '</div></div>';
+                
                 echo '</div>';
                 echo '</div>';
                 echo '</div>';
@@ -137,8 +142,11 @@
 
         if (!$printSection) {
             var $printSection = document.createElement("div");
-            $printSection.style.width = "10cm";
-            $printSection.style.height = "24cm";
+            $printSection.style.width = "794px";
+            $printSection.style.height = "960px";
+
+            // $printSection.style.width = "10cm";
+            // $printSection.style.height = "24cm";
             $printSection.id = "printSection";
             document.body.appendChild($printSection);
         }
