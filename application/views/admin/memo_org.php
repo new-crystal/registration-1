6d1fb658abf86f1b314abcf535eae7da037d9483
new-crystal <oj3339@naver.com>
2025-03-12 15:48:11 +0900

    [250312] modify org

--- application/views/admin/memo_org.php
+++ application/views/admin/memo_org.php
@@ -1,11 +1,11 @@
 <script src="https://cdn.tailwindcss.com"></script>
 
-<?php echo form_open('/admin/memo_nickname?n=' . $item['registration_no'], 'id="memoForm" name="memoForm"');
+<?php echo form_open('/admin/memo_org?n=' . $item['registration_no'], 'id="memoForm" name="memoForm"');
 ?>
 <div class="flex flex-col items-center justify-center w-full h-full">
-    <p>변경하실 성함을 입력해주세요.</p>
-    <input id="nickname" name="nickname" class="w-10/12 h-4/6 border p-5"
-        value="<?php echo isset($item['nick_name']) && $item['nick_name'] != 'null' ? $item['nick_name'] : ''; ?>">
+    <p>변경하실 소속을 입력해주세요.</p>
+    <input id="org" name="org" class="w-10/12 h-4/6 border p-5"
+        value="<?php echo isset($item['org_nametag']) && $item['org_nametag'] != 'null' ? $item['org_nametag'] : ''; ?>">
     </input>
     <div class="flex items-center justify-center w-full mt-5">
         <button type="submit" id="save" class="h-8 w-28 bg-pink-600 text-white mx-5">Save</button>
@@ -16,7 +16,7 @@
 <script>
 const closeButton = document.querySelector("#close")
 const saveButton = document.querySelector("#save")
-const memo = document.querySelector("#nickname")
+const memo = document.querySelector("#org")
 const form = document.querySelector("#memoForm")
 
 closeButton.addEventListener("click", () => {
@@ -35,9 +35,9 @@ async function saveMemo() {
     const registration_no = window.location.search.split("=")[1];
 
     if (memoValue === "") {
-        formData.set('nickname', null); // 메모 필드의 값을 공백으로 설정
+        formData.set('org', null); // 메모 필드의 값을 공백으로 설정
     } else {
-        formData.set('nickname', memoValue);
+        formData.set('org', memoValue);
     }
     try {
         const response = await fetch(url, {
@@ -47,16 +47,16 @@ async function saveMemo() {
 
         if (response.ok) {
             // 메모 저장 성공
-            alert("성함 저장 성공");
+            alert("소속 저장 성공");
             const parentWindow = window.opener;   
             window.opener.postMessage(registration_no, "*");
             window.close();
         } else {
             // 메모 저장 실패
-            alert("성함 저장 실패");
+            alert("소속 저장 실패");
         }
     } catch (error) {
-        console.log("성함 저장 중 오류 발생:", error);
+        console.log("소속 저장 중 오류 발생:", error);
     }
 }
 </script>
